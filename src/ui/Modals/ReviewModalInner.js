dc3edcd472681a24864f837d18785ac90e4c3989
Manav Gupta <2006468@kiit.ac.in>
2023-03-25 17:44:54 +0530

    updated modal page

--- src/ui/Modals/ReviewModalInner.js
+++ src/ui/Modals/ReviewModalInner.js
@@ -6,6 +6,32 @@ const ReviewModalInner = (props) => {
   console.log(props.mainData);
   return (
     <div id={props.id} className="ReviewModal_inner">
+      <div>
+        <div>Model risk management document</div>
+        <div>
+          Credit line {props.mainData.creditLineData.mode} model : Version{" "}
+          {props.mainData.creditLineData.version}
+        </div>
+        <div>Model group: {props.mainData.modalGrp}</div>
+        <div>
+          <p>date created: {props.mainData.createDate}</p>
+        </div>
+        <div>
+          <p>Developer: {props.mainData.developerName}</p>
+        </div>
+        {props.mainData.selected === "yes" && (
+          <div>
+            <div>Reviewer: {props.mainData.reviewStatus.name}</div>
+            <div>last review date: {props.mainData.reviewStatus.date}</div>
+          </div>
+        )}
+      </div>
+      <div>
+        {/* {props.mainData.modalSpecificData.map((each) => {
+          return <div>{each.title}</div>
+          <div>{each.desc}</div>
+        })} */}
+      </div>
       <LineChart srdata={props.exceldata.utilization} labelName="Utilization" />
     </div>
   );
